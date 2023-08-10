1144cb8c8bfc9eda6368e180956b692bc7ac8f3b
MFindley08 <123705549+MFindley08@users.noreply.github.com>
2023-08-10 19:18:47 -0400

    added Dictionary.js and implemented initial form function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./images/dictionary-logo.png";
 
 function App() {
   return (
-    <div className="App-container">
+    <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="dictionary-logo " alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
       <Footer />
+    </div>
     </div>
   );
 }
