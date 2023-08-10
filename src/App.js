15a7e130cdcc9877401c150d698af8873ee67f0a
MFindley08 <123705549+MFindley08@users.noreply.github.com>
2023-08-10 16:13:55 -0400

    added logo and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./images/dictionary-logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Dictionary</h1>
+      <header className="App-header">
+        <img src={logo} className="dictionary-logo " alt="logo" />
+      </header>
     </div>
   );
 }
