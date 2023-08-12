c462133c67f286af35c0aeb2638213c61ce0f8d7
MFindley08 <123705549+MFindley08@users.noreply.github.com>
2023-08-12 19:53:57 -0400

    added css and phonetics and default word

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./images/dictionary-logo.png";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-        <img src={logo} className="dictionary-logo " alt="logo" />
-      </header>
-      <main>
-        <Dictionary />
-      </main>
-      <Footer />
-    </div>
+        <header className="App-header">
+          <img src={logo} className="dictionary-logo " alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="pumpkin" />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
-
-export default App;
